c8db9083205f473dc2f8aa661a7410f08201ffd5
VinayReddy18 <142021280+VinnuReddy18@users.noreply.github.com>
2024-07-03 11:28:04 +0530

    CRUD

--- index.js
+++ index.js
@@ -1,24 +1,60 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Hello World');
-});
+app.use(express.json());
 
-let courses = [
-    { id: 1, name: "java" },
-    { id: 2, name: "javascript" },
-    { id: 3, name: "python" },
-    { id: 4, name: "php"}
+let courses = [ {
+    id: 1,
+    name: 'javascript'
+}, {
+    id: 2,
+    name: 'nodejs'
+}, {
+    id: 3,
+    name: 'express'
+}
+
 ];
 
-app.get('/courses',(req,res) => {
+app.get('/courses', (req, res) => {
     res.json(courses);
-});
-app.post('/courses',(req,res)=>{
-    console.log(req.body);
 })
 
-app.listen(3001, () => {
-    console.log('Listening on port 3000');
+app.post('/courses', (req, res) => {
+    const course = {
+        id: courses.length + 1,
+        name: req.body.name
+    }
+    courses.push(course);
+    res.json(course);
+    console.log("added");
+}
+)
+app.put('/courses/:id', (req, res) => {
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    if (!course) {
+        res.status(404).send('The course with the given ID was not found');
+    }
+
+    course.name = req.body.name;
+    res.json(course);
+}
+)
+
+app.delete('/courses/:id', (req, res) => {
+    const courseId = parseInt(req.params.id);
+    const course = courses.find(c => c.id === courseId);
+
+    if (!course) {
+        return res.status(404).send('The course with the given ID was not found');
+    }
+
+    const index = courses.indexOf(course);
+    courses.splice(index, 1);
+
+    res.json(course);
 });
+
+app.listen(3000,()=>{
+    console.log("Server is running on port 3000");
+})
